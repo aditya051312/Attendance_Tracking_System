57894e15abca10734fc2f2b976d0acd9c62b9505
aditya051312 <adityakhillari1@gmail.com>
2024-05-30 13:53:14 +0530

    Added App.jsx for the Attendance Tracking System.

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import LoginPage from './LoginPage';
+
+function App() {
+  return (
+    <Router>
+      <Route exact path="/">
+        <Redirect to="/login" />
+      </Route>
+      <Route path="/login" component={LoginPage} />
+    </Router>
+  );
+}
+
+export default App;
