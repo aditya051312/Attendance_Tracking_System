c25e69176bcb58160dfbb2bcc273acd7bb10d96f
aditya051312 <adityakhillari1@gmail.com>
2024-08-27 18:36:40 +0530

    Added App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import LoginPage from './LoginPage';
+
+const App = () => (
+  <Router>
+    <Route exact path="/">
+      <Redirect to="/login" />
+    </Route>
+    <Route path="/login" component={LoginPage} />
+  </Router>
+);
+
+export default App;
